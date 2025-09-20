c2126a20a454005546aa8dfdc5c1ed917a75f10e
techcoderx <git@techcoderx.com>
2025-09-20 23:54:22 +0800

    lib: put raw ipld block

--- lib/ipfs_dag/src/lib.rs
+++ lib/ipfs_dag/src/lib.rs
@@ -1,14 +1,19 @@
-use libipld::{ block::Block, cbor::DagCborCodec, codec::Encode, multihash::Code::Sha2_256, store::DefaultParams };
+use libipld::{ block::Block, cbor::DagCborCodec, codec::Encode, multihash::Code::Sha2_256, raw::RawCodec, store::DefaultParams };
 
 pub fn put_dag<T>(payload: &T) -> String where T: Encode<DagCborCodec> + ?Sized {
   let a = Block::<DefaultParams>::encode(DagCborCodec, Sha2_256, payload).unwrap();
   a.cid().to_string()
 }
 
+pub fn put_dag_raw<T>(payload: &T) -> String where T: Encode<RawCodec> + ?Sized {
+  let a = Block::<DefaultParams>::encode(RawCodec, Sha2_256, payload).unwrap();
+  a.cid().to_string()
+}
+
 #[cfg(test)]
 mod tests {
   use std::fs;
-  use super::put_dag;
+  use super::{ put_dag, put_dag_raw };
 
   #[test]
   fn test_put_dag_string() {
@@ -19,5 +24,12 @@ mod tests {
   fn test_put_dag_file() {
     let file = fs::read("test/build.wasm").unwrap();
     assert_eq!(put_dag(file.as_slice()).as_str(), "bafyreihmqkn5sql6zts7rpgf7iwuatpb4yj3tpw2gx7akpsucawy2hlbwm");
+  }
+
+  #[test]
+  fn test_put_dag_file_raw() {
+    // vsc1Bem8RnoLgGPP7E2MBN52ekrdVqy2LNpSqF
+    let file = fs::read("test/hello-world.wasm").unwrap();
+    assert_eq!(put_dag_raw(file.as_slice()).as_str(), "bafkreibwwj3fypek5uz6l3scacy47yw3b2adgbxmfab2ybmkarljaggbey");
   }
 }
